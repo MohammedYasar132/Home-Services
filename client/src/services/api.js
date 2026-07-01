50093f39cfeb23f65ffb8211b54e733e0e194604
Mohammed Yasar <mohammedyasar132@email.com>
2026-07-01 12:47:29 +0530

    NodeMailer -> Resend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const API = axios.create({
-  baseURL: "https://home-services-jyvw.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,21 +1,15 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import dotenv from "dotenv";
 
 dotenv.config();
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS ? "PASS FOUND" : "NO PASS");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (subject, html) => {
-  await transporter.sendMail({
-    from: `"Home Services" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "Home Services <onboarding@resend.dev>",
     to: process.env.EMAIL_USER,
     subject,
     html,
